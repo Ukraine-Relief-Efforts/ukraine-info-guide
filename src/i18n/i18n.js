a8857a7ee46cec7f26d1eda630ff8a73c4559d2e
Ollie Etherington <ollie@etherington.xyz>
2022-02-25 18:15:15 +0000

    Split translations into separate files and lazy-load them

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,17 +1,20 @@
 import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import resources from './translations';
 
 i18n
+	.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
-		resources,
 		fallbackLng: 'en',
 		debug: process.env.NODE_ENV === 'development',
 		interpolation: {
 			escapeValue: false,
+		},
+		react: {
+			useSuspense: false,
 		},
 	});
 
