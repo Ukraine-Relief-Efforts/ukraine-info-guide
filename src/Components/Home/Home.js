7dbaac2858041d0cbe167c8ae0415485f87bfee3
Oulsen <47483877+Oulsen@users.noreply.github.com>
2022-02-25 19:28:49 +0100

    changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,6 @@ import BorderCard from "../Country/Country";
 function Home() {
   return (
     <section className="w-full flex flex-col items-center justify-center mt-5">
-      <p className="text-2xl font-bold">MAIN PAGE</p>
       <div className="flex flex-col flex-wrap mt-10">
         <BorderCard />
       </div>
