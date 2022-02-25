ab66b9f2d85285cafa1e87be0bae0fe8268d47fa
Oulsen <47483877+Oulsen@users.noreply.github.com>
2022-02-25 18:30:05 +0100

    Simple navbar with main page thing

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,8 @@
+function Navbar() {
+  return (
+    <section className="w-full flex items-center justify-center mt-5">
+      <p className="text-2xl font-bold">MAIN PAGE</p>
+    </section>
+  );
+}
+export default Navbar;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <section>
+      <nav className="navbar">
+        <div className="navbar-item navbar-item-active">
+          <a className="">Leave Ukraine</a>
+        </div>
+        <div className="navbar-item">
+          <a className="">Border information</a>
+        </div>
+      </nav>
+    </section>
+  );
+}
+export default Navbar;
