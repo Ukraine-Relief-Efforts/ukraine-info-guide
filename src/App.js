f088a9a945e876da19c704205d90e434b0da45f3
plotka <juliaplotka93@gmail.com>
2022-02-25 18:22:15 +0100

    Add basic routing with pages examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
+import ServicesPage from "./pages/services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/services">Services</Link>
+      </nav>
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="services" element={<ServicesPage />} />
+      </Routes>
     </div>
   );
 }
