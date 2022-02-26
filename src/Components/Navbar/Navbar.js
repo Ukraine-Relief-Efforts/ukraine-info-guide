fb67165719095688f9de15128921074da30e0cbf
Ollie Etherington <ollie@etherington.xyz>
2022-02-26 09:33:33 +0000

    Better Navbar mobile menu

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import menuSvg from "../../assets/menu.svg";
+import closeSvg from "../../assets/close.svg";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setCurrentPage, openMenu } from "../../globalState/slices/currentPage";
 import LanguagePicker from "./LanguagePicker";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 const Link = ({ title, url }) => {
   const dispatch = useDispatch();
@@ -32,37 +35,53 @@ const Links = ({ t }) => {
   );
 }
 
-const MobileMenu = ({ page }) => {
-  const dispatch = useDispatch();
+const MobileMenuButton = ({ page, toggleMenu }) => {
   return (
     <img
       src={menuSvg}
-      alt="menu"
-      width={40}
-      onClick={() => dispatch(openMenu({ menuOpen: !page.menuOpen }))}
-      className="cursor-pointer"
+      alt="Open menu"
+      width={35}
+      onClick={toggleMenu}
+      className="cursor-pointer mr-4 ml-4"
     />
   );
 }
 
 const Navbar = () => {
-  const page = useSelector((state) => state.currentPage);
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const page = useSelector((state) => state.currentPage);
+
+  const toggleMenu = () => dispatch(openMenu({ menuOpen: !page.menuOpen }))
+
   return (
     <section>
       <div className="bg-gray-200 py-5 flex flex-row items-center">
         <div className="grow ml-2 text-lg">LeaveUkraine.com</div>
         <LanguagePicker />
-        <MobileMenu page={page} />
-        <div className={page.menuOpen ? "block" : "hidden"}>
-          <nav className="navbar-mobile">
-            <Links t={t} />
-          </nav>
-        </div>
+        <MobileMenuButton {...{ page, toggleMenu }} />
         <nav className="navbar">
           <Links t={t} />
         </nav>
       </div>
+      <Drawer
+        open={page.menuOpen}
+        onClose={toggleMenu}
+        direction="right"
+      >
+        <nav className="navbar-mobile">
+          <div className="w-full">
+            <img
+              src={closeSvg}
+              alt="Close menu"
+              width={40}
+              onClick={toggleMenu}
+              className="cursor-pointer float-right mt-4 mr-4"
+            />
+          </div>
+          <Links t={t} />
+        </nav>
+      </Drawer>
     </section>
   );
 }
