037e7f3ee024a42ed8c13d2c2990b943a1a79019
Ollie Etherington <ollie@etherington.xyz>
2022-02-26 19:51:13 +0000

    Add translations for poland form

--- src/pages/poland-form/index.js
+++ src/pages/poland-form/index.js
@@ -5,124 +5,132 @@ import { useTranslation } from "react-i18next";
 
 const PolandFormPage = () => {
   const { t } = useTranslation();
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
   return (
     <Layout>
-      <Hero title="Form to apply for Asylum in Poland" />
+      <Hero title={t("Form to apply for Asylum in Poland")} />
       <div>
         <form onSubmit={handleSubmit}>
           <fieldset>
-            <h1>Personal Data</h1>
+            <h1 className="text-lg mt-6 mb-3">{t("Personal Data")}</h1>
             <label>
-              <p>First Name</p>
+              <p>{t("First Name")}</p>
               <input name="firstName" />
             </label>
             <label>
-              <p>Last Name</p>
+              <p>{t("Last Name")}</p>
               <input name="lastName" />
             </label>
             <label>
-              <p>Maiden Name</p>
+              <p>{t("Maiden Name")}</p>
               <input name="maidenName" />
             </label>
             <label>
-              <p>Father's Name</p>
+              <p>{t("Father's Name")}</p>
               <input name="fathersName" />
             </label>
             <label>
-              <p>Mother's Name</p>
+              <p>{t("Mother's Name")}</p>
               <input name="mothersName" />
             </label>
             <label>
-              <p>Date of Birth</p>
+              <p>{t("Date of Birth")}</p>
               <input name="dateOfBirth" />
             </label>
             <label>
-              <p>Sex</p>
+              <p>{t("Sex")}</p>
               <input name="sex" />
             </label>
             <label>
-              <p>Place of Birth</p>
+              <p>{t("Place of Birth")}</p>
               <input name="placeOfBirth" />
             </label>
             <label>
-              <p>Country of Birth</p>
+              <p>{t("Country of Birth")}</p>
               <input name="countryOfBirth" />
             </label>
             <label>
-              <p>Citizenships</p>
+              <p>{t("Citizenships")}</p>
               <input name="citizenships" />
             </label>
             <label>
-              <p>Marital status</p>
+              <p>{t("Marital status")}</p>
               <input name="maritalStatus" />
             </label>
             <label>
-              <p>Height</p>
+              <p>{t("Height")}</p>
               <input name="height" />
             </label>
             <label>
-              <p>Color of Eyes</p>
+              <p>{t("Color of Eyes")}</p>
               <input name="colorOfEyes" />
             </label>
             <label>
-              <p>Special Marks</p>
+              <p>{t("Special Marks")}</p>
               <input name="specialMarks" />
             </label>
             <label>
-              <p>Polish Identification Number</p>
+              <p>{t("Polish Identification Number")}</p>
               <input name="polishIdentificationNumber" />
             </label>
-            <h1>Travel Documents</h1>
+            <h1 className="text-lg mt-6 mb-3">{t("Travel Documents")}</h1>
             <label>
-              <p>Series</p>
+              <p>{t("Series")}</p>
               <input name="series" />
             </label>
             <label>
-              <p>Number</p>
+              <p>{t("Number")}</p>
               <input name="number" />
             </label>
             <label>
-              <p>Date of issue</p>
+              <p>{t("Date of issue")}</p>
               <input name="dateOfIssue" />
             </label>
             <label>
-              <p>Date of Expiry</p>
+              <p>{t("Date of Expiry")}</p>
               <input name="dateOfExpiry" />
             </label>
             <label>
-              <p>Issuing Authority</p>
+              <p>{t("Issuing Authority")}</p>
               <input name="issuingAuthority" />
             </label>
             <label>
-              <p>Number of entered persons</p>
+              <p>{t("Number of entered persons")}</p>
               <input name="numberOfEnteredPersons" />
             </label>
-            <h1>Place of Stay</h1>
+            <h1 className="text-lg mt-6 mb-3">{t("Place of Stay")}</h1>
             <label>
-              <p>City</p>
+              <p>{t("City")}</p>
               <input name="city" />
             </label>
             <label>
-              <p>Street</p>
+              <p>{t("Street")}</p>
               <input name="street" />
             </label>
             <label>
-              <p>Building Number</p>
+              <p>{t("Building Number")}</p>
               <input name="buildingNumber" />
             </label>
             <label>
-              <p>Apartment number</p>
+              <p>{t("Apartment number")}</p>
               <input name="apartmentNumber" />
             </label>
             <label>
-              <p>Postal Code</p>
+              <p>{t("Postal Code")}</p>
               <input name="postalCode" />
             </label>
           </fieldset>
-          <button type="submit">Sumbit</button>
+          <button
+            type="submit"
+            className="mt-4 mb-20 p-3 rounded"
+            style={{ backgroundColor: "#999" }}
+          >
+            {t("Sumbit")}
+          </button>
         </form>
       </div>
     </Layout>
