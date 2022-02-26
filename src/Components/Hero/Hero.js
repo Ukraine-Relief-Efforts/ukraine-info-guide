96c5b0ab2ae58491bd33d82478295f3affeb001d
plotka <juliaplotka93@gmail.com>
2022-02-27 00:08:10 +0100

    Add padding to hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,6 @@
 const Hero = ({ title, subcomponent = null }) => {
   return (
-    <div className="w-full shadow-md bg-[#FFF199]">
+    <div className="w-full shadow-md bg-[#FFF199] p-2">
       <div className="max-w-6xl mx-auto justify-center items-center flex mb-5 py-16">
         <div className="items-center">
           <p className="text-4xl text-bold text-black text-center">{title}</p>
