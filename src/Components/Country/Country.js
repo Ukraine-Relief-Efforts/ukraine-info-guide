1ff0d19c4a98864d7632851a3fa2827755774454
stigakl <stigakl@outlook.com>
2022-02-26 01:28:59 +0100

    contrast changes on the information page

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -5,18 +5,22 @@ import data from "../../dummydata/poland_data.json";
 function Country() {
   return (
     <section className="border-card">
-      <p className="text-2xl font-semibold">Poland</p>
+      {/*<p className="text-2xl font-semibold">Poland</p>*/}
       <p className="text-xl font-semibold mb-5">Help for Ukrainian citizens</p>
-      <ul className="text-center">
-        <p className="font-semibold">INFORMATION FOR UKRAINE CITIZENS</p>
-        {data.general.map((item) => {
-          return (
-            <li className="border-card-point" key={item}>
-              {item}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="bg-gray-200 p-3">
+        <ul className="border-information-info-list list-disc">
+          <p className="font-semibold mb-5">
+            INFORMATION FOR UKRAINE CITIZENS WHO ARE TRYING TO TRAVEL TO POLAND
+          </p>
+          {data.general.map((item) => {
+            return (
+              <li className="mb-2" key={item}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <div className="flex flex-wrap w-full items-center justify-center">
         {data.reception.map((item) => {
           return <ReceptionPoint data={item} />;
