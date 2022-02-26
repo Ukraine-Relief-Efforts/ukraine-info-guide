f367e04d936e2e07916d6f76d4999d2e6180edbf
plotka <juliaplotka93@gmail.com>
2022-02-26 23:46:49 +0100

    Comment out some links due to lack of space on the navbar

--- src/Components/Navbar/Links.js
+++ src/Components/Navbar/Links.js
@@ -7,12 +7,12 @@ import Link from "./Link";
 const Links = ({ t }) => {
   return (
     <>
-      <Link title={t("Leave Ukraine")} url="/" />
+      {/* <Link title={t("Leave Ukraine")} url="/" /> */}
       <Link title={t("Border Info")} url="/border-information" />
       <Link title={t("Food and Shelter")} url="/food-and-shelter" />
       <Link title={t("Polish Asylum Form")} url="/poland-form" />
       <Link title={t("About")} url="/about" />
-      <Link title={t("Services")} url="/services" />
+      {/* <Link title={t("Services")} url="/services" /> */}
       <LoginButton />
       <LogoutButton />
       <AuthenticatedUserProfile />
