712dded330ca8505e13edccc88e27de5978abb71
stigakl <stigakl@outlook.com>
2022-02-26 01:28:44 +0100

    more interactive link

--- src/Pages/PolandBorder.js
+++ src/Pages/PolandBorder.js
@@ -1,11 +1,14 @@
 const PolandBorderInfo = () => (
-  <div className=" justify-center items-center flex mb-5 py-5 bg-yellow-400 w-full">
+  <div className=" justify-center items-center flex mb-5 mt-10 py-5 bg-yellow-400 w-full">
     <div className=" items-center">
       <p className="text-2xl text-bold text-red-500 text-center">
         How to cross Poland's border
       </p>
       <p className="text-xl text-center">
-        <a className="text-red" href="https://www.gov.pl/web/udsc/ukraina-en">
+        <a
+          className="text-red border-information"
+          href="https://www.gov.pl/web/udsc/ukraina-en"
+        >
           &#128073; www.gov.pl &#128072;
         </a>
       </p>
