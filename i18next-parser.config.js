64a32097237c21fe5fc8eab6e23bb6fb24f26c1c
Elecarno <54546385+ElecarnoYT@users.noreply.github.com>
2022-02-26 21:09:34 +0000

    Delete i18next-parser.config.js

--- i18next-parser.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	keepRemoved: false,
-	js: ['JsxLexer'],
-	jsx: ['JsxLexer'],
-	locales: ['en', 'ua', 'ru', 'it', 'de', 'es'],
-	output: 'public/locales/$LOCALE/$NAMESPACE.json',
-	input: [ 'src/**/*.js', 'src/**/*.jsx' ],
-};
