d9c7e869bae6d0d63bebb972ed0710434938e4f2
Tony-Kwon-Tonykeycapstore <100478659+Tony-Kwon-Tonykeycapstore@users.noreply.github.com>
2022-02-28 00:49:40 +0900

    Update i18next-parser.config.js

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	keepRemoved: false,
 	js: ['JsxLexer'],
 	jsx: ['JsxLexer'],
-	locales: ['en', 'ua', 'ru', 'it', 'de', 'es', 'pl'],
+	locales: ['en', 'ua', 'ru', 'it', 'de', 'es', 'pl', 'kr'],
 	output: 'public/locales/$LOCALE/$NAMESPACE.json',
 	input: [ 'src/**/*.js', 'src/**/*.jsx' ],
 };
