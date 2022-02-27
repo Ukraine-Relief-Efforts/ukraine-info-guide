c39360c87aa5f9b611a06a6a1d04653751a874e2
Ollie Etherington <ollie@etherington.xyz>
2022-02-27 09:00:27 +0000

    Remove globalState

--- src/globalState/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import currentPage from "./slices/currentPage";
-
-export const store = configureStore({
-  reducer: {
-    currentPage,
-  },
-});
