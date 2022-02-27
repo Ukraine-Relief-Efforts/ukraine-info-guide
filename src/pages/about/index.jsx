fe51127d9816ce15f32663b11dc2e5a1fb77f175
Miguel <rawredonion@gmail.com>
2022-02-27 18:42:39 -0500

    Testing OG support without SSR on Netlify.

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,21 +2,39 @@ import { useTranslation, Trans } from "react-i18next";
 import Hero from "../../components/Hero";
 import Layout from "../../components/Layout";
 import { GITHUB_URL } from "../../configs/constants";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const AboutPage = () => {
   const { t } = useTranslation();
 
   return (
-    <Layout hero={<Hero title={t("About us")} />}>
-      <p>
-        <Trans t={t} i18nKey="Join us <1>on Github</1>">
-          Join us <a
-            href={GITHUB_URL}
-            className="link"
-          >on Github</a>
-        </Trans>
-      </p>
-    </Layout>
+    <HelmetProvider>
+      <div>
+        <Helmet>
+          <title>About Us - Leave Ukraine</title>
+          <meta
+            property="og:title"
+            content="About Us - Leave Ukraine"
+            data-rh="true"
+          ></meta>
+          <meta
+            property="og:description"
+            content="Information about website and the team behind it."
+            data-rh="true"
+          ></meta>
+        </Helmet>
+        <Layout hero={<Hero title={t("About Us")} />}>
+          <p>
+            <Trans t={t} i18nKey="Join us <1> on Github</1>">
+              Join us
+              <a href={GITHUB_URL} className="link">
+                on Github
+              </a>
+            </Trans>
+          </p>
+        </Layout>
+      </div>
+    </HelmetProvider>
   );
 };
 
