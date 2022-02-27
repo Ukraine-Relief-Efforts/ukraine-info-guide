fc4868d0d6e16f0f4421a759ef3b179c9872f7a4
Ollie Etherington <ollie@etherington.xyz>
2022-02-27 09:03:36 +0000

    Remove unused import

--- src/hooks/useCountryData.js
+++ src/hooks/useCountryData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { fetchCountryBorderInfo } from "../api/CountryApi";
 
