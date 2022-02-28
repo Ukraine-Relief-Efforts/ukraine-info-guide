9e2ebd9ef513684a6303ca0c368379c27116c459
Ollie Etherington <ollie@etherington.xyz>
2022-02-28 17:38:33 +0000

    Fix navbar overflowing

--- components/Navbar/Link.js
+++ components/Navbar/Link.js
@@ -2,7 +2,7 @@ import NavLink from "next/link";
 import { useRouter } from "next/router";
 import { useToggle } from "./ToggleContext";
 
-const Link = ({ title, url }) => {
+const Link = ({ title, url, customStyle }) => {
   const router = useRouter();
   const isActive = router.pathname === url;
   const { setIsMenuOpen } = useToggle();
@@ -11,7 +11,7 @@ const Link = ({ title, url }) => {
     <NavLink href={url}>
       <a
         className={`navbar-item ${isActive ? "navbar-item-active" : ""}`}
-        style={{ padding: "0.3em 0.5em", fontSize: "1em" }}
+        style={{ padding: "0.3em 0.5em", fontSize: "1em", ...customStyle }}
         onClick={() => setIsMenuOpen(false)}
       >
         {title}
