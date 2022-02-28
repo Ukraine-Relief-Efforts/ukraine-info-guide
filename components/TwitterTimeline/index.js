efc602d0ad17eb40f699f15b7c16c7857010a752
Ollie Etherington <ollie@etherington.xyz>
2022-02-28 20:42:51 +0000

    Add loading spinner for Twitter timeline

--- components/TwitterTimeline/index.js
+++ components/TwitterTimeline/index.js
@@ -1,11 +1,15 @@
 import Script from "next/script";
+import Spinner from "../Spinner";
 
 const TwitterTimeline = () => {
   return (
-    <>
-      <a className="twitter-timeline" href="https://twitter.com/TechForUkraine?ref_src=twsrc%5Etfw">Tweets by TechForUkraine</a>
+    <div style={{ width: "300px", textAlign: "center" }}>
+      <a className="twitter-timeline" href="https://twitter.com/TechForUkraine?ref_src=twsrc%5Etfw">
+        <Spinner />
+        Loading tweets
+      </a>
       <Script async src="https://platform.twitter.com/widgets.js" />
-    </>
+    </div>
   );
 }
 
