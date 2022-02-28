a914190a8ded9c870e9b638e1f244e4ceb95f424
Ollie Etherington <ollie@etherington.xyz>
2022-02-28 21:10:53 +0000

    Fix Twitter timeline not showing when navigating for a second time

--- components/TwitterTimeline/index.js
+++ components/TwitterTimeline/index.js
@@ -1,15 +1,19 @@
-import Script from "next/script";
+import Head from "next/head";
 import Spinner from "../Spinner";
 
 const TwitterTimeline = () => {
   return (
-    <div style={{ width: "300px", textAlign: "center" }}>
-      <a className="twitter-timeline" href="https://twitter.com/TechForUkraine?ref_src=twsrc%5Etfw">
-        <Spinner />
-        Loading tweets
-      </a>
-      <Script async src="https://platform.twitter.com/widgets.js" />
-    </div>
+    <>
+      <Head>
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </Head>
+      <div style={{ width: "300px", textAlign: "center" }}>
+        <a className="twitter-timeline" href="https://twitter.com/TechForUkraine?ref_src=twsrc%5Etfw">
+          <Spinner />
+          Loading tweets
+        </a>
+      </div>
+    </>
   );
 }
 
