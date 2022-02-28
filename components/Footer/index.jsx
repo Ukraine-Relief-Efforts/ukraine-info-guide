b6233082cb2f2f5e29043600ec87dcda7bd1744d
Ollie Etherington <ollie@etherington.xyz>
2022-02-28 17:54:28 +0000

    Refactor footer links and headers into separate components

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import FooterHeader from "./FooterHeader";
+import FooterLink from "./FooterLink";
 
 const Footer = () => {
   return (
@@ -7,41 +9,16 @@ const Footer = () => {
         <div className="mx-6 py-10 text-center md:text-left">
           <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
             <div>
-              <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">Useful links</h6>
-              <p className="mb-4">
-                <Link href="/border-information">
-                  <a className="text-gray-600">Border Info</a>
-                </Link>
-              </p>
-              <p className="mb-4">
-                <Link href="/">
-                  <a className="text-gray-600">Host a refugee</a>
-                </Link>
-              </p>
+              <FooterHeader>Useful links</FooterHeader>
+              <FooterLink href="/border-information">Border Info</FooterLink>
+              <FooterLink href="/#">Host a refugee</FooterLink>
             </div>
-
             <div>
-              <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">Contacts</h6>
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <Link href="/">
-                  <a className="text-gray-600">Twitter</a>
-                </Link>
-              </p>
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <Link href="/">
-                  <a className="text-gray-600">Facebook</a>
-                </Link>
-              </p>
-              <p className="flex items-center justify-center md:justify-start mb-4">
-                <Link href="/">
-                  <a className="text-gray-600">Telegram</a>
-                </Link>
-              </p>
-              <p className="flex items-center justify-center md:justify-start">
-                <Link href="/">
-                  <a className="text-gray-600">Instagram</a>
-                </Link>
-              </p>
+              <FooterHeader>Contacts</FooterHeader>
+              <FooterLink href="/#">Twitter</FooterLink>
+              <FooterLink href="/#">Facebook</FooterLink>
+              <FooterLink href="/#">Telegram</FooterLink>
+              <FooterLink href="/#">Instagram</FooterLink>
             </div>
           </div>
         </div>
