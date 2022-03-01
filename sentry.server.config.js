3ae75ae421994a0c7edcb86cb64ee3ff89d3b9ef
Konrad <konrad@labk.it>
2022-03-01 20:51:54 +0100

    Changed project

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://654f7eef323c4a40b74d63f2cb7b3b9f@o1156401.ingest.sentry.io/6237673',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
