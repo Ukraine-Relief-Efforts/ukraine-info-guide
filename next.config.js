4c74d1a1d26d643a8aea1915f0b887a670566817
Konrad <konrad@labk.it>
2022-03-01 20:19:40 +0100

    Added sentry

--- next.config.js
+++ next.config.js
@@ -1,21 +1,38 @@
 const withPrefresh = require("@prefresh/next");
-const { i18n } = require("./next-i18next.config");
+const {i18n} = require("./next-i18next.config");
+const {withSentryConfig} = require("@sentry/nextjs");
 
-module.exports = withPrefresh({
-  i18n,
-  reactStrictMode: true,
-  webpack(config) {
-    Object.assign(config.resolve.alias, {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
-    });
+const moduleExports = withPrefresh({
+    i18n,
+    reactStrictMode: true,
+    webpack(config) {
+        Object.assign(config.resolve.alias, {
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
+        });
 
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        });
 
-    return config;
-  }
+        return config;
+    }
 });
+
+const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
