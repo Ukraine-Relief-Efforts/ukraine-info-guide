e48f4471f71ce6700aebe28634345445d7c221af
Ollie Etherington <ollie@etherington.xyz>
2022-03-02 08:35:33 +0000

    Alerts page content

--- components/TelegramAlertLinks/index.js
+++ components/TelegramAlertLinks/index.js
@@ -8,10 +8,10 @@ const makeUrl = (locale) =>
 const TelegramAlertLinks = ({ title }) => (
   <div className="text-center mt-5">
     <p>{title}</p>
-    <div className="flex flex-row space-x-4 mt-3">
+    <div className="flex flex-row flex-wrap mt-3 justify-center">
       {
         languages.map(({ value, name }) =>
-          <a className="link" href={makeUrl(value)}>{name}</a>
+          <a className="link m-3" href={makeUrl(value)}>{name}</a>
         )
       }
     </div>
--- pages/alerts.js
+++ pages/alerts.js
@@ -1,9 +1,11 @@
+import { useState, useRef, useEffect } from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import MissileAlerts from "../components/MissileAlerts";
 import TelegramAlertLinks from "../components/TelegramAlertLinks";
+import features from "../configs/features";
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
@@ -13,6 +15,20 @@ export const getStaticProps = async ({ locale }) => ({
 
 const AlertsPage = () => {
   const { t } = useTranslation();
+
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    if (scriptLoaded || !ref.current)
+      return;
+    const tag = document.createElement("script");
+    tag.src = "https://telegram.org/js/telegram-widget.js?15"
+    tag.setAttribute("data-telegram-post", "LeaveUkraineAlertsUK/47");
+    tag.setAttribute("data-width", "100%");
+    ref.current.appendChild(tag);
+    setScriptLoaded(true);
+  }, [scriptLoaded, ref.current]);
 
   return (
     <Layout
@@ -25,15 +41,28 @@ const AlertsPage = () => {
         />
       }
     >
-      <p>{t("We offer Telegram channels in 9 languages for live notifications of missile and air alerts")}</p>
-      <p>{t("Choose your language below")}:</p>
-      <div>
-        <MissileAlerts />
-      </div>
-      <div className="my-10">
-        <TelegramAlertLinks
-          title={t("Join us on telegram to view older alerts")}
-        />
+      <div className="text-center px-5" style={{ maxWidth: "60ch" }}>
+        <p className="text-lg">
+          {t("We offer Telegram channels in 9 languages for live notifications of missile and air alerts accross Ukraine")}
+        </p>
+        <div className="my-10">
+          <TelegramAlertLinks
+            title={t("Choose your language")}
+          />
+        </div>
+        <div ref={ref} style={{ maxWidth: "400px", margin: "0 auto" }} />
+        {features.liveMissileAlerts &&
+          <>
+            <div>
+              <MissileAlerts />
+            </div>
+            <div className="my-10">
+              <TelegramAlertLinks
+                title={t("Join us on telegram to view older alerts")}
+              />
+            </div>
+          </>
+        }
       </div>
     </Layout>
   );
