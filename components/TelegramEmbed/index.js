016b8bd6388addf2e9b294b75767b3557033aaa1
Ollie Etherington <ollie@etherington.xyz>
2022-03-03 08:47:41 +0000

    Display the most recent telegram alert instead of a hard-coded post number

---
+++ components/TelegramEmbed/index.js
@@ -0,0 +1,38 @@
+import { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+import { localeToTelegramAlertChannel } from "../../utils";
+import { fetchMissileAlerts } from "../../apiClient/AlertsApi";
+
+const TelegramEmbed = () => {
+  const router = useRouter();
+
+  const [postNumber, setPostNumber] = useState();
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchMissileAlerts(router.locale, 1);
+      setPostNumber(data?.alerts[0]?.post);
+    };
+    fetchData();
+  });
+
+  useEffect(() => {
+    if (scriptLoaded || !ref.current || !postNumber)
+      return;
+    const channel = localeToTelegramAlertChannel(router.locale);
+    const tag = document.createElement("script");
+    tag.src = "https://telegram.org/js/telegram-widget.js?15"
+    tag.setAttribute("data-telegram-post", `${channel}/${postNumber}`);
+    tag.setAttribute("data-width", "100%");
+    ref.current.appendChild(tag);
+    setScriptLoaded(true);
+  }, [postNumber]);
+
+  return (
+    <div ref={ref} style={{ maxWidth: "400px", margin: "0 auto" }} />
+  );
+}
+
+export default TelegramEmbed;
--- pages/api/v1/missileAlerts.js
+++ pages/api/v1/missileAlerts.js
@@ -1,8 +1,6 @@
 import cheerio from "cheerio";
 import { localeToTelegramAlertChannel } from "../../../utils";
 
-const NUM_RESULT_TO_SHOW = 10;
-
 const readDOM = async (locale) => {
   const channel = localeToTelegramAlertChannel(locale);
   const data = await fetch(`https://t.me/s/${channel}`);
@@ -11,24 +9,32 @@ const readDOM = async (locale) => {
   }
 
 const missileAlertsHandler = async (req, res) => {
+  const num_results = req.query.count || 10;
+
   try {
     const $ = await readDOM(req.query.lang || "en");
-    const messages = $(".tgme_widget_message_bubble");
+    const messages = $(".tgme_widget_message");
 
     const result = [];
 
     messages.each((i, elem) => {
       elem = $(elem);
+
       const text = elem.find(".tgme_widget_message_text").html();
       const time = elem.find(".time").html();
+
+      const dataPost = elem[0]?.attribs["data-post"];
+      const post = parseInt(dataPost.split("/")[1]);
+
       result.push({
         text,
         time,
+        post,
       });
     });
 
     res.status(200).json({
-      alerts: result.slice(result.length - NUM_RESULT_TO_SHOW),
+      alerts: result.slice(result.length - num_results),
     });
   } catch (e) {
     res.status(500).json({ error: "Couldn't get missile alerts" + e });
