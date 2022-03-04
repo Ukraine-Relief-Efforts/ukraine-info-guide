08ed1da1c52e55197b8df543257809ffab111aea
Ollie Etherington <ollie@etherington.xyz>
2022-03-04 17:11:55 +0000

    Set Twitter timeline language correctly

--- components/TwitterTimeline/index.js
+++ components/TwitterTimeline/index.js
@@ -1,13 +1,17 @@
+import { useRouter } from "next/router";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 import Spinner from "../Spinner";
 import { TWITTER_HANDLE } from "../../configs/constants";
 
 const TwitterTimeline = () => {
+  const { locale } = useRouter();
+
   return (
     <div style={{ width: "300px" }}>
       <TwitterTimelineEmbed
         sourceType="profile"
         screenName={TWITTER_HANDLE}
+        lang={locale}
         placeholder={
           <div style={{ width: "300px", textAlign: "center" }}>
             <Spinner />
