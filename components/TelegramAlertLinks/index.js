ee07835c08de1e2c4eb771f5df7af5b13e6c2bc1
Ollie Etherington <ollie@etherington.xyz>
2022-03-04 09:26:39 +0000

    Default to the user's language in TelegramAlertLinks

--- components/TelegramAlertLinks/index.js
+++ components/TelegramAlertLinks/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { localeToTelegramAlertChannel } from "../../utils";
 import languages from "../../configs/languages";
 
@@ -7,19 +9,62 @@ const channels = languages.filter(({ value }) =>
 const makeUrl = (locale) =>
   `https://t.me/${localeToTelegramAlertChannel(locale)}`;
 
-const TelegramAlertLinks = ({ title }) => (
-  <div className="text-center mt-5">
+const ChannelLink = ({ value, name, className }) => (
+  <a className={`link m-3 ${className}`} href={makeUrl(value)}>
+    {name}
+  </a>
+);
+
+const AllChannels = ({ title }) => (
+  <div className="flex flex-row flex-wrap mt-3 justify-center">
     <p>{title}</p>
-    <div className="flex flex-row flex-wrap mt-3 justify-center">
+    <div>
       {
-        channels.map(({ value, name }, index) =>
-          <a className="link m-3" href={makeUrl(value)} key={index}>
-            {name}
-          </a>
-        )
+        channels.map((locale, index) =>
+          <ChannelLink key={index} {...locale} />)
       }
     </div>
   </div>
 );
+
+const ChannelsWithDefault = ({ defaultChannel, title }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  const toggle = () => setShowAll(!showAll);
+
+  return (
+    <div className="mb-10">
+      <ChannelLink className="font-xl font-semibold" {...defaultChannel} />
+      <p className="mt-8">
+        <a
+          className="text-gray-600 cursor-pointer text-sm select-none"
+          onClick={toggle}
+        >
+          {title} {showAll ? <>&#9662;</> : <>&#9664;</>}
+        </a>
+      </p>
+      {showAll && <AllChannels />}
+    </div>
+  );
+}
+
+const TelegramAlertLinks = ({ title }) => {
+  const { locale } = useRouter();
+
+  const defaultChannel = channels.find(({ value }) => value === locale);
+
+  return (
+    <div className="text-center mt-5">
+      {
+        defaultChannel
+          ? <ChannelsWithDefault
+              defaultChannel={defaultChannel}
+              title={title}
+            />
+          : <AllChannels title={title} />
+      }
+    </div>
+  );
+}
 
 export default TelegramAlertLinks;
