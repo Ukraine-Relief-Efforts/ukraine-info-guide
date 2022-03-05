6013d09b0e7c800031ad21480a6e34b99308342e
Ollie Etherington <ollie@etherington.xyz>
2022-03-05 22:23:46 +0000

    Update the local translation file on submit

--- pages/api/v1/translations.js
+++ pages/api/v1/translations.js
@@ -1,4 +1,12 @@
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import {
+  readFile as readFileCallback,
+  writeFile as writeFileCallback,
+} from "fs";
+import { promisify } from "util";
+
+const readFile = promisify(readFileCallback);
+const writeFile = promisify(writeFileCallback);
 
 const translationsHandler = async (req, res) => {
   if (req.method !== "POST") {
@@ -8,6 +16,20 @@ const translationsHandler = async (req, res) => {
 
   const { user } = getSession(req, res);
   const { data, lang } = req.body;
+
+  for (const key in data)
+    data[key] = data[key].trim();
+
+  const path = `./public/locales/${lang}/translation.json`;
+
+  const oldData = JSON.parse(await readFile(path));
+
+  const newData = { ...oldData, ...data };
+
+  const output = JSON.stringify(newData, null, 2);
+
+  await writeFile(path, output);
+
 
   res.json({ updated: 0 });
 }
