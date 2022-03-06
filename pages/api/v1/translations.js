b065d274fcc44b499a3bd4a961715271f914f0ad
Ollie Etherington <ollie@etherington.xyz>
2022-03-06 16:57:18 +0000

    Simplify translation update logic

--- pages/api/v1/translations.js
+++ pages/api/v1/translations.js
@@ -1,13 +1,5 @@
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import nodemailer from "nodemailer";
-import {
-  readFile as readFileCallback,
-  writeFile as writeFileCallback,
-} from "fs";
-import { promisify } from "util";
-
-const readFile = promisify(readFileCallback);
-const writeFile = promisify(writeFileCallback);
 
 const translationsHandler = async (req, res) => {
   if (req.method !== "POST") {
@@ -21,15 +13,7 @@ const translationsHandler = async (req, res) => {
   for (const key in data)
     data[key] = data[key].trim();
 
-  const path = `./public/locales/${lang}/translation.json`;
-
-  const oldData = JSON.parse(await readFile(path));
-
-  const newData = { ...oldData, ...data };
-
-  const output = JSON.stringify(newData, null, 2);
-
-  await writeFile(path, output);
+  const content = JSON.stringify(data, null, 2);
 
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
@@ -49,7 +33,7 @@ const translationsHandler = async (req, res) => {
     attachments: [
       {
         filename: "translation.json",
-        path,
+        content,
       },
     ],
   });
