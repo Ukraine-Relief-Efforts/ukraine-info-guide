a1066b5508d78f1b03afc297c43fdd3a21a84416
Ollie Etherington <ollie@etherington.xyz>
2022-03-08 22:10:06 +0000

    CountryDataView2 small improvements

--- components/CountryDataView/CountryDataView2.js
+++ components/CountryDataView/CountryDataView2.js
@@ -4,6 +4,7 @@ import LocationCard from "./LocationCard";
 import Spinner from "../Spinner";
 import Map from "../Map";
 import CountryPicker from "../CountryPicker";
+import NewTabLink from "../NewTabLink";
 
 const Attribution = ({ children }) => (
   <p className="my-3 opacity-70 text-right">
@@ -37,6 +38,11 @@ const CountryDataView2 = ({
   return (
     <section className="country-data-view">
       {hasMap && <Map markers={reception} kmlUrl={kmlUrl} mapRef={mapRef} />}
+      <div className="text-right mr-5 mb-10">
+        <NewTabLink className="link" href="https://www.google.com/maps/d/u/0/viewer?mid=1s5tABxCBpgKYxMi1GHqt8zFAisiz7qRF&hl=en_US&ll=47.60277360304404%2C28.4859373561754&z=6">
+          {t("Source")}
+        </NewTabLink>
+      </div>
       <div className="bg-gray-200 p-3 rounded pt-10">
         <p className="text-xl font-semibold text-blue-ukraine text-center">
           {t("Choose a country")}:
@@ -49,11 +55,13 @@ const CountryDataView2 = ({
         </div>
         <ul className="country-data-view-info-list list-disc">
           {!error && general &&
-            general.map((item, index) => (
-              <li className="mb-2" key={index}>
-                {item}
-              </li>
-            ))
+            general.length
+              ? general.map((item, index) => (
+                <li className="mb-2" key={index}>
+                  {item}
+                </li>
+              ))
+              : <Spinner />
           }
         </ul>
         {source && source.length > 0 &&
