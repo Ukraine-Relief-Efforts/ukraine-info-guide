e89cabc19f0e38d80e0f270b17526a696f3b75f1
Ollie Etherington <ollie@etherington.xyz>
2022-03-10 09:10:27 +0000

    Add curfew info

--- pages/index.js
+++ pages/index.js
@@ -45,12 +45,41 @@ const IndexPage = () => {
     onClick: () => router.push("/resources"),
   });
 
+  const curfews = [
+    { region: t("Kyiv region"), time: "19:00 - 7:00" },
+    { region: t("Dnipropetrovsk region"), time: "20:00 - 6:00" },
+    { region: t("Cherkasy region"), time: "22:00 - 6:00" },
+    { region: t("Vinnytsia region"), time: "22:00 - 7:00" },
+    { region: t("Zhytomyr region"), time: "22:00 - 6:00" },
+    { region: t("Zaporizhzhia region"), time: "22:00 - 6:00" },
+    { region: t("Ivano-Frankivsk region"), time: "22:00 - 8:00" },
+    { region: t("Mykolaiv region"), time: "22:00 - 6:00" },
+    { region: t("Odessa region"), time: "19:00 - 6:00" },
+    { region: t("Poltava region"), time: "22:00 - 6:00" },
+    { region: t("Rivne region"), time: "22:00 - 6:00" },
+    { region: t("Kharkiv region"), time: "18:00 - 6:00" },
+    { region: t("Cherkasy region"), time: "22:00 - 6:00" },
+    { region: t("Chernihiv region"), time: "22:00 - 6:00" },
+    { region: t("Kherson region"), time: "22:00 - 6:00" },
+    { region: t("Khmelnytsky region"), time: "22:00 - 6:00" },
+  ];
+
   return (
     <Layout hero={
       <Hero title={t("Leave Ukraine")} subcomponent={<Description t={t} />} />}
     >
       <div className="flex flex-col w-full mb-10">
         <IconPicker options={options} />
+      </div>
+      <div className="flex flex-col items-center mt-10">
+        <p className="font-semibold text-xl mb-2">{t("Curfews")}</p>
+        {
+          curfews.map(({ region, time }, index) => (
+            <div key={index}>
+              {region} - {time}
+            </div>
+          ))
+        }
       </div>
     </Layout>
   );
