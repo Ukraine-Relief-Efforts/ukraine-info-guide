5c8f9e7a629505ef0b516daf68dd9ccb64af55ea
Ollie Etherington <ollie@etherington.xyz>
2022-03-11 17:25:29 +0000

    Parse KML files on the backend

---
+++ backend/KmlParser.js
@@ -0,0 +1,56 @@
+import jsdom from "jsdom";
+
+class KmlParser {
+	constructor(kml) {
+		this.dom = new jsdom.JSDOM(kml, "text/xml");
+	}
+
+	toJson(normalizedNames) {
+		const places = this.dom.window.document.querySelectorAll("Placemark");
+
+		const output = [];
+
+		for (const place of places) {
+			const result = {};
+
+			result.name = place.querySelector("name")?.innerHTML;
+
+			const data = place.querySelector("ExtendedData")?.children || [];
+			for (const item of data) {
+				const itemName = item.attributes?.name?.value;
+				const itemValue = item.querySelector("value")?.innerHTML;
+				result[normalizedNames[itemName] || itemName] = itemValue;
+			}
+
+			const coords = result.coords;
+			if (coords) {
+				const parts = coords.split(", ");
+				result.lat = parts[0];
+				result.lon = parts[1];
+				result.position = [
+					parseFloat(result.lat),
+					parseFloat(result.lon),
+				];
+
+				result.color = KmlParser.getColor(result.color);
+
+				output.push(result);
+			}
+		}
+
+		return output;
+	}
+
+	static getColor(c) {
+		if (typeof c === "string") {
+			c = c.toLowerCase();
+			if (c.indexOf("green") > -1) return "green";
+			if (c.indexOf("orange") > -1) return "orange";
+			if (c.indexOf("red") > -1) return "red";
+			if (c.indexOf("tbd") > -1 || c.indexOf("blue") > -1) return "blue";
+		}
+		return "black";
+	}
+}
+
+export default KmlParser;
--- pages/api/v1/country-kml.js
+++ pages/api/v1/country-kml.js
@@ -5,11 +5,12 @@ import {
   unlinkSync,
 } from "fs";
 import { promisify } from "util";
+import KmlParser from "../../../backend/KmlParser";
 
 const readFile = promisify(readFileSync);
 const writeFile = promisify(writeFileSync);
 
-const regex = /country-kml-.*\.kml/;
+const regex = /country-kml-.*\.json/;
 
 const kmlUrl= "https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1s5tABxCBpgKYxMi1GHqt8zFAisiz7qRF";
 
@@ -18,8 +19,27 @@ const getLocalPath = () => {
   const month = now.getMonth();
   const day = now.getDay();
   const hour = now.getHours();
-  return `./country-kml-${month}-${day}-${hour}.kml`;
+  return `./country-kml-${month}-${day}-${hour}.json`;
 }
+
+const normalizedNames = {
+  "CONTACT NO.": "phone",
+  "Colour": "color",
+  "CODE": "color",
+  "Coordinates": "coords",
+  "GMAPS COORDINATES": "coords",
+  "GEOLOCATION": "coords",
+  "DATE LAST CONTACTED": "dateLastContacted",
+  "TIME LAST CONTACTED": "timeLastContacted",
+  "DELAY BY BUS": "delayByBus",
+  "Затримка автобусом": "delayByBus",
+  "DELAY BY CAR": "delayByCar",
+  "Затримка на машині": "delayByCar",
+  "DELAY BY FOOT": "delayByFoot",
+  "Затримка пішки": "delayByFoot",
+  "Дані про трафік": "trafficData",
+  "Рекомендований час прибуття": "recommendedTime",
+};
 
 const countryKmlHandler = async (req, res) => {
   if (req.method !== "GET") {
@@ -31,14 +51,16 @@ const countryKmlHandler = async (req, res) => {
 
   try {
     const cached = await readFile(path);
-    res.status(200).send(cached);
+    res.status(200).json(JSON.parse(cached));
     return;
   } catch (e) {} // eslint-disable-line
 
   try {
     const result = await fetch(kmlUrl);
     const text = await result.text();
-    res.status(200).send(text);
+    const parser = new KmlParser(text);
+    const json = parser.toJson(normalizedNames);
+    res.status(200).json(json);
     console.log("Fetched new countries kml");
 
     try {
@@ -46,14 +68,14 @@ const countryKmlHandler = async (req, res) => {
         .filter(f => regex.test(f))
         .map(f => unlinkSync(`./${f}`));
 
-      await writeFile(path, text);
+      await writeFile(path, JSON.stringify(json));
     } catch (e) {
       console.log("Failed to cache countries kml:", e);
     }
   } catch (e) {
     try {
-      const text = await readFile("./public/ukraine-border.kml");
-      console.error("Failed to fetch new KML data - using cache instead");
+      const text = await readFile("./public/country-kml-backup.json");
+      console.error("Failed to fetch new KML data - using backup instead");
       res.status(200).send(text);
     } catch (e) {
       res.status(500).send("Couldn't fetch KML");
